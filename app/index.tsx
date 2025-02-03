7164f2a04f803a03cea28312afdd9abccc200700
Makhare01 <maxare2015@gmail.com>
2025-02-03 11:34:21 +0400

    init: project basic structure

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const MainView = () => {
+  return (
+    <View>
+      <Text>Main View</Text>
+    </View>
+  );
+};
+
+export default MainView;
---
+++ app/quiz.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const QuizView = () => {
+  return (
+    <View>
+      <Text>Quiz View</Text>
+    </View>
+  );
+};
+
+export default QuizView;
